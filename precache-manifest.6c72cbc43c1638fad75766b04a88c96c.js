f6a5c40c605dcdbce25a9d62a4ec3ab66739d3a9
Rúben Rebelo <rav.rebelo@campus.fct.unl.pt>
2019-11-23 21:37:13 +0000

    Updates

---
+++ precache-manifest.6c72cbc43c1638fad75766b04a88c96c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3318f3e8621dee6e340b7c52c20e88",
+    "url": "/ipm-foodie-app/index.html"
+  },
+  {
+    "revision": "e94ff1dd88cca61690a4",
+    "url": "/ipm-foodie-app/static/css/main.188bc96d.chunk.css"
+  },
+  {
+    "revision": "8c9f51c8ddc71bddded1",
+    "url": "/ipm-foodie-app/static/js/2.c988f7bb.chunk.js"
+  },
+  {
+    "revision": "e94ff1dd88cca61690a4",
+    "url": "/ipm-foodie-app/static/js/main.942ad004.chunk.js"
+  },
+  {
+    "revision": "ad3cd68ab234eddea170",
+    "url": "/ipm-foodie-app/static/js/runtime-main.7aa31d6b.js"
+  },
+  {
+    "revision": "f2f5fb8a6c7dd339c1ba18db493647cf",
+    "url": "/ipm-foodie-app/static/media/delete_frog.f2f5fb8a.png"
+  },
+  {
+    "revision": "f640524f8668d8cb3f5186a58b71fe0b",
+    "url": "/ipm-foodie-app/static/media/frog_favorite.f640524f.png"
+  },
+  {
+    "revision": "f6249409d13701cf75791196386d974a",
+    "url": "/ipm-foodie-app/static/media/homepage_frog.f6249409.png"
+  },
+  {
+    "revision": "6922359a4f764fa5072a742df16b620b",
+    "url": "/ipm-foodie-app/static/media/logo.6922359a.png"
+  }
+]);
